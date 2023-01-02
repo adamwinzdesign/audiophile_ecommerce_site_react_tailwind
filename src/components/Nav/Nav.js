ec4b0f30d8ace466185d7c2c8e6eaa8b177c0f8a
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-02 13:01:28 -0600

    updating Nav.js and tailwind.config.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,31 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
 	return (
-		<nav className='flex gap-[10px]'>
-			<Link to='/'>Home</Link>
-			<Link to='/headphones'>Headphones</Link>
-			<Link to='/speakers'>Speakers</Link>
-			<Link to='/earphones'>Earphones</Link>
+		<nav className='h-[5.625rem] desk:h-[6rem] flex items-center justify-center gap-[2.125rem] bg-black'>
+			<Link
+				to='/'
+				className='font-bold text-[13px] leading-[25px] tracking-[2px] uppercase text-white hover:text-peru duration-500'
+			>
+				Home
+			</Link>
+			<Link
+				to='/headphones'
+				className='font-bold text-[13px] leading-[25px] tracking-[2px] uppercase text-white hover:text-peru duration-500'
+			>
+				Headphones
+			</Link>
+			<Link
+				to='/speakers'
+				className='font-bold text-[13px] leading-[25px] tracking-[2px] uppercase text-white hover:text-peru duration-500'
+			>
+				Speakers
+			</Link>
+			<Link
+				to='/earphones'
+				className='font-bold text-[13px] leading-[25px] tracking-[2px] uppercase text-white hover:text-peru duration-500'
+			>
+				Earphones
+			</Link>
 		</nav>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./components/**/*.{js,jsx,ts,tsx}",
-		"./pages/**/*.{js,jsx,ts,tsx}",
+		// components and pages would be necessary for nextJS, but src is for CRA
+		// "./components/**/*.{js,jsx,ts,tsx}",
+		// "./pages/**/*.{js,jsx,ts,tsx}",
 		"./src/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				heroBGMobile: "url(../public/images/home/mobile/image-header.jpg)",
+				zx9Rings: "url(../public/images/home/desktop/pattern-circles.svg)",
+			},
+			backgroundSize: {
+				944: "944px",
+			},
+			colors: {
+				peru: "#D87D4A",
+				very_light_tangelo: "#FBAF85",
+				black: "#000",
+				white: "#FFF",
+				border_grey: "#979797",
+			},
+			screens: {
+				desk: "1440px",
+			},
+		},
 	},
 	plugins: [],
 };
