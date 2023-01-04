4a4d9ae4ed870ef63a364714cf1de4b8c9e7d34c
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-04 13:01:50 -0600

    updating Footer spacing at tablet width

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,30 +4,35 @@ import twitter from "../../images/shared/desktop/icon-twitter.svg";
 import instagram from "../../images/shared/desktop/icon-instagram.svg";
 import StyledLink from "../Nav/StyledLink";
 
+// left off with spacing on footer at tablet width.  Spacing has been updated/specified, but was rendered incorrect when we had to group the logo and links together to switch to flex row for the links at desktop width
+
 const Footer = () => {
 	return (
-		<div className='relative h-[40.875rem] md:h-[25rem] desk:h-[22.875rem] w-full pt-[3.25rem] md:pt-[3.75rem] pb-[2.375rem] md:pb-[2.875rem] px-[1.6rem] md:px-[2.375rem] desk:px-[10.375rem] flex flex-col items-center md:items-start md:justify-between gap-[3rem] md:gap-0 bg-black'>
+		<div className='relative h-[40.875rem] md:h-[25rem] desk:h-[22.875rem] w-full pt-[3.25rem] md:pt-[3.75rem] desk:pt-[4.75rem] pb-[2.375rem] md:pb-[2.875rem] desk:pb-[3.5rem] px-[1.6rem] md:px-[2.375rem] desk:px-[10.375rem] flex flex-col items-center md:items-start md:justify-between gap-[3rem] md:gap-0 bg-black'>
 			{/* brand accent */}
 			<div className='absolute top-0 md:left-[2.375rem] desk:left-[10.375rem] w-[101px] h-[4px] bg-peru' />
-			{/* logo */}
-			<img src={logo} alt='audiophile logo' className='w-[9rem]' />
-			{/* links */}
-			<div className='flex flex-col md:flex-row items-center gap-[1rem] md:gap-[2.125rem]'>
-				<StyledLink dest='/' text='Home' />
-				<StyledLink dest='/headphones' text='Headphones' />
-				<StyledLink dest='/speakers' text='Speakers' />
-				<StyledLink dest='/earphones' text='Earphones' />
+			{/* logo and links */}
+			<div className='w-full flex flex-col items-center desk:flex-row desk:justify-between gap-[3rem] md:gap-0'>
+				{/* logo */}
+				<img src={logo} alt='audiophile logo' className='w-[9rem]' />
+				{/* links */}
+				<div className='flex flex-col md:flex-row items-center gap-[1rem] md:gap-[2.125rem]'>
+					<StyledLink dest='/' text='Home' />
+					<StyledLink dest='/headphones' text='Headphones' />
+					<StyledLink dest='/speakers' text='Speakers' />
+					<StyledLink dest='/earphones' text='Earphones' />
+				</div>
 			</div>
 			{/* info */}
 			<div>
-				<p className='text-[15px] leading-[25px] text-white/50 text-center md:text-left md:pb-[3.2rem]'>
+				<p className='desk:w-[33rem] text-[15px] leading-[25px] text-white/50 text-center md:text-left md:pb-[3.2rem]'>
 					Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music
 					lovers and sound specialists who are devoted to helping you get the most out of personal
 					audio. Come and visit our demo facility - we’re open 7 days a week.
 				</p>
 			</div>
 			{/* copyright and socials */}
-			<div className='w-full flex flex-col md:flex-row items-center md:justify-between'>
+			<div className='relative w-full flex flex-col md:flex-row items-center md:justify-between'>
 				{/* copyright */}
 				<div>
 					<p className='text-[15px] leading-[25px] text-white/50 text-center'>
@@ -35,7 +40,7 @@ const Footer = () => {
 					</p>
 				</div>
 				{/* socials */}
-				<div className='flex gap-[1rem]'>
+				<div className='desk:absolute desk:right-0 desk:bottom-[5rem] flex gap-[1rem]'>
 					<img src={facebook} alt='facebook' />
 					<img src={twitter} alt='twitter' />
 					<img src={instagram} alt='instagram' />
