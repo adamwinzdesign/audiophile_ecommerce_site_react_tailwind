3ad792a111893e8f7911520b38019ebfcec5df7b
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-05 14:28:50 -0600

    updated HomeYX1Card for tablet

--- src/pages/Home/HomeYX1Card.js
+++ src/pages/Home/HomeYX1Card.js
@@ -1,19 +1,20 @@
 const HomeYX1Card = () => {
 	return (
-		<div className='pb-[5rem] flex flex-col items-start'>
+		<div className='pb-[5rem] md:pb-0 flex flex-col md:flex-row items-start md:items-center'>
 			<div
 				className='
-					h-[12.5rem] w-full 
+					h-[12.5rem] md:h-[20rem] w-full md:max-w-[21.25rem]
 					bg-cover bg-no-repeat bg-homeYX1BGMobile md:bg-homeYX1BGTablet rounded-lg
 				'
 			/>
-
-			<h2 className='mt-[3.5rem] mb-[1.5rem] mx-[1.625rem] font-bold text-[1.75rem] leading-[3.375rem] tracking-[2px] uppercase text-black'>
-				YX1 Earphones
-			</h2>
-			<button className='w-[10rem] py-[1rem] mx-[1.625rem] font-bold text-[1rem] leading-[1.125rem] tracking-[1px] uppercase border border-black bg-transparent'>
-				See Product
-			</button>
+			<div className='mt-[3.5rem] mb-[1.5rem] mx-[1.625rem] md:mx-[2.75rem]'>
+				<h2 className='mb-[1.5rem] font-bold text-[1.75rem] leading-[3.375rem] tracking-[2px] uppercase text-black'>
+					YX1 Earphones
+				</h2>
+				<button className='w-[10rem] py-[1rem] font-bold text-[1rem] leading-[1.125rem] tracking-[1px] uppercase border border-black bg-transparent'>
+					See Product
+				</button>
+			</div>
 		</div>
 	);
 };
