0d61aa4fad3c98a59d31233d577fac215c4afcb0
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-08 15:44:14 -0600

    updating HomeHero for tablet

--- src/pages/Home/HomeHero.js
+++ src/pages/Home/HomeHero.js
@@ -2,19 +2,19 @@ const HomeHero = () => {
 	return (
 		<div
 			className='
-				w-[100vw] h-[32rem] 
-				bg-[center_bottom_0.1rem] bg-cover bg-no-repeat bg-black bg-homeHeroBGMobile 
+				w-[100vw] h-[32rem] md:h-[40rem]
+				bg-[center_bottom_0.1rem] bg-cover bg-no-repeat bg-black bg-homeHeroBGMobile md:bg-homeHeroBGTablet
 				flex items-center justify-center
 				'
 		>
-			<div className='w-[87.5%] h-[18rem] flex flex-col items-center justify-between'>
+			<div className='w-[87.5%] h-[18rem] flex flex-col items-center justify-between border border-red-600'>
 				<p className='text-[14px] leading-[19px] tracking-[10px] text-center uppercase text-white/50'>
-					new products
+					new product
 				</p>
-				<h2 className='font-bold text-[2.25rem] leading-[2.5rem] tracking-[1.28px] text-center text-white uppercase'>
+				<h2 className='font-bold text-[2.25rem] md:text-[3.5rem] leading-[2.5rem] md:leading-[3.625rem] tracking-[1.28px] text-center text-white uppercase'>
 					XX99 mark II headphones
 				</h2>
-				<p className='font-medium text-[15px] leading-[1.5rem] text-center text-white/75'>
+				<p className='md:max-w-[22rem] font-medium text-[15px] leading-[1.5rem] text-center text-white/75'>
 					Experience natural, lifelike audio and exceptional build quality made for the passionate
 					music enthusiast.
 				</p>
