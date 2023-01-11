d0e2e9064d78efa72fe88ffa24be62e268d73884
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-11 10:20:33 -0600

    updated Nav with min-w at desktop to correct spacing issues with the desktop links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,15 +7,15 @@ const Nav = ({ handleMenuClick }) => {
 	return (
 		<nav className='px-[1.5rem] md:px-[2.5rem] desk:px-[10.25rem] h-[5.625rem] desk:h-[6rem] bg-black'>
 			<div className='w-full h-full flex items-center justify-between border-b-[1px] border-white/20'>
-				<div className='min-w-[30%] desk:hidden w-[16px] h-[15px] cursor-pointer'>
+				<div className='desk:hidden w-[16px] h-[15px] cursor-pointer'>
 					<MenuButton handleMenuClick={handleMenuClick} />
 				</div>
 
-				<div className='min-w-[30%]'>
+				<div className='desk:min-w-[30%] flex justify-start'>
 					<img src={logo} alt='audiophile logo' />
 				</div>
 
-				<div className='min-w-[30%] hidden desk:flex'>
+				<div className='desk:min-w-[30%] hidden desk:flex'>
 					<div className='hidden desk:flex gap-[2.125rem]'>
 						<StyledLink dest='/' text='Home' />
 						<StyledLink dest='/headphones' text='Headphones' />
@@ -24,7 +24,7 @@ const Nav = ({ handleMenuClick }) => {
 					</div>
 				</div>
 
-				<div className='min-w-[30%] flex justify-end'>
+				<div className='desk:min-w-[30%] flex justify-end'>
 					<img src={cart} alt='cart' />
 				</div>
 			</div>
