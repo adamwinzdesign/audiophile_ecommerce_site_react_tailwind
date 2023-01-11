fa715a411c33a1205bcfd0b033ce11076f55fc65
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-11 13:37:04 -0600

    updated EarphonesCategory to filter and display earphones category data

--- src/pages/Earphones/EarphonesCategory.js
+++ src/pages/Earphones/EarphonesCategory.js
@@ -1,9 +1,16 @@
 import PageFadeIn from "../../components/PageFadeIn";
+import allProductData from "../../utils/data.json";
+import CategoryHeader from "../../components/ProductCategories/CategoryHeader";
+import CategoryList from "../../components/ProductCategories/CategoryList";
 
 const EarphonesCategory = () => {
+	const allCategoryProductData = allProductData.filter(
+		(product) => product.category === "earphones"
+	);
 	return (
 		<PageFadeIn>
-			<h1>EarphonesCategory</h1>
+			<CategoryHeader text='Earphones' />
+			<CategoryList allCategoryProductData={allCategoryProductData} />
 		</PageFadeIn>
 	);
 };
