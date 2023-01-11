c5b80924cc271eab7bec0c8f0157e2193d15c0c4
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-11 13:32:13 -0600

    updated SpeakersCategory to filter and display category info for speakers

--- src/pages/Speakers/SpeakersCategory.js
+++ src/pages/Speakers/SpeakersCategory.js
@@ -1,9 +1,16 @@
 import PageFadeIn from "../../components/PageFadeIn";
+import CategoryHeader from "../../components/ProductCategories/CategoryHeader";
+import CategoryList from "../../components/ProductCategories/CategoryList";
+import allProductData from "../../utils/data.json";
 
 const SpeakersCategory = () => {
+	const allCategoryProductData = allProductData.filter(
+		(product) => product.category === "speakers"
+	);
 	return (
 		<PageFadeIn>
-			<h1 className='text-black'>SpeakersCategory</h1>
+			<CategoryHeader text='Speakers' />
+			<CategoryList allCategoryProductData={allCategoryProductData} />
 		</PageFadeIn>
 	);
 };
