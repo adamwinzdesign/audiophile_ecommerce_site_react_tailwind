c5dfd6b9e92b04dfdd2de4a59e770b18c5a8696a
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-15 16:56:26 -0600

    updating Nav, added indicator to cart to display number of items in cart, needs positioning

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,13 @@
 import MenuButton from "./MenuButton";
 import StyledLink from "./StyledLink";
 import logo from "../../images/shared/desktop/logo.svg";
-import cart from "../../images/shared/desktop/icon-cart.svg";
+import cartIcon from "../../images/shared/desktop/icon-cart.svg";
+import { useCartStore } from "../../store/cartStore";
+import PageFadeIn from "../PageFadeIn";
 
 const Nav = ({ handleMenuClick }) => {
+	const { cart } = useCartStore();
+
 	return (
 		<nav className='px-[1.5rem] md:px-[2.5rem] desk:px-[10.25rem] h-[5.625rem] desk:h-[6rem] bg-black'>
 			<div className='w-full h-full flex items-center justify-between border-b-[1px] border-white/20'>
@@ -24,8 +28,15 @@ const Nav = ({ handleMenuClick }) => {
 					</div>
 				</div>
 
-				<div className='desk:min-w-[30%] flex justify-end'>
-					<img src={cart} alt='cart' />
+				<div className='desk:min-w-[30%] flex justify-end overflow-visible'>
+					<img src={cartIcon} alt='cart' />
+					{/* {cart.length && (
+						<PageFadeIn>
+							<div className='absolute top-[0.75rem] left-[0.75rem] h-[1.2rem] w-[1.2rem] flex items-center justify-center rounded-full bg-peru'>
+								<p className='text-[14px] text-white'>{cart.length}</p>
+							</div>
+						</PageFadeIn>
+					)} */}
 				</div>
 			</div>
 		</nav>
