8a1269147498aa28bb2669aa09d32a2a44012c1c
adamwinzdesign <adamwinzdesign@gmail.com>
2023-01-26 13:30:30 -0600

    added /Home/HomeCategoryCard, updated HomeCategoryLinks, needed to remove the partial background from the category cards on the home screen but leave in place everywhere else

--- src/pages/Home/HomeCategoryLinks.js
+++ src/pages/Home/HomeCategoryLinks.js
@@ -1,6 +1,5 @@
-// re-using the MobileCard element from MobileMenu.  We aren't using the entire MobileMenu component because it is a motion.div there and we don't want or need any of the animation props here
-
 import MobileCard from "../../components/Nav/MobileCard";
+import HomeCategoryCard from "./HomeCategoryCard";
 import headphones from "../../images/shared/desktop/image-category-thumbnail-headphones.png";
 import speakers from "../../images/shared/desktop/image-category-thumbnail-speakers.png";
 import earphones from "../../images/shared/desktop/image-category-thumbnail-earphones.png";
@@ -15,9 +14,12 @@ const HomeCategoryLinks = () => {
 				flex flex-col md:flex-row justify-between md:items-center desk:gap-[2rem]
 			'
 		>
-			<MobileCard img={headphones} title='headphones' dest='/headphones' />
+			{/* <MobileCard img={headphones} title='headphones' dest='/headphones' />
 			<MobileCard img={speakers} title='speakers' dest='/speakers' />
-			<MobileCard img={earphones} title='earphones' dest='/earphones' />
+			<MobileCard img={earphones} title='earphones' dest='/earphones' /> */}
+			<HomeCategoryCard img={headphones} title='headphones' dest='/headphones' />
+			<HomeCategoryCard img={speakers} title='speakers' dest='/speakers' />
+			<HomeCategoryCard img={earphones} title='earphones' dest='/earphones' />
 		</div>
 	);
 };
